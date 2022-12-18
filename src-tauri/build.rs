e26a8e80f7c5dbec6c426ac61e4320d6e0461c0c
César Santos <cesar.santos.1990@gmail.com>
2022-12-19 00:45:57 +0100

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,18 +5,19 @@ fn main() {
     let dst = Config::new("../src-cpp").build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=backend");
-    println!("cargo:rustc-link-lib=static=gecode-solver");
   
     // needed for gecode
-    println!("cargo:rustc-link-lib=static=gecodekernel");
     println!("cargo:rustc-link-lib=static=gecodesupport");
-    println!("cargo:rustc-link-lib=static=gecodedriver");
+    println!("cargo:rustc-link-lib=static=gecodekernel");
+    println!("cargo:rustc-link-lib=static=gecodesearch");
     println!("cargo:rustc-link-lib=static=gecodeint");
     println!("cargo:rustc-link-lib=static=gecodeset");
     println!("cargo:rustc-link-lib=static=gecodefloat");
-    println!("cargo:rustc-link-lib=static=gecodesearch");
+    println!("cargo:rustc-link-lib=static=gecodedriver");
     println!("cargo:rustc-link-lib=static=gecodeminimodel");
+
+    println!("cargo:rustc-link-lib=static=gecode-solver");
+    println!("cargo:rustc-link-lib=static=backend");
     
     tauri_build::build()
 }
