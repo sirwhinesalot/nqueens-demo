3d12ad6ec17ab733bf334e2d523c04e972234eb8
César Santos <cesar.santos.1990@gmail.com>
2022-12-19 00:16:40 +0100

    Fixed memcpy import on linux

--- src-cpp/src/backend.cpp
+++ src-cpp/src/backend.cpp
@@ -2,6 +2,7 @@
 #include "gecode-solver.hpp"
 
 #include <mutex>
+#include <cstring>
 
 Solver* solver_new(SolverId id) {
     // TODO: only gecode for now, so no need to check the id
@@ -20,7 +21,7 @@ size_t solver_get_num_solutions(Solver* solver) {
 void solver_get_solution(Solver* solver, size_t index, uint8_t* result) {
     std::lock_guard<std::mutex> guard(solver->mutex);
     auto solution_ref = solver->solutions.at(index);
-    memcpy(result, solution_ref.data(), solution_ref.size());
+    std::memcpy(result, solution_ref.data(), solution_ref.size());
 }
 
 void solver_cancel(Solver* solver) {
