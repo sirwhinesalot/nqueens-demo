9f73f6c219a738767240ab32707b381eec5241c5
César Santos <cesar.santos.1990@gmail.com>
2022-12-19 15:11:49 +0100

    Reordered library links for g++

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,17 +6,18 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
   
-    // needed for gecode
-    println!("cargo:rustc-link-lib=static=gecodesupport");
-    println!("cargo:rustc-link-lib=static=gecodekernel");
-    println!("cargo:rustc-link-lib=static=gecodesearch");
-    println!("cargo:rustc-link-lib=static=gecodeint");
-    println!("cargo:rustc-link-lib=static=gecodeset");
-    println!("cargo:rustc-link-lib=static=gecodefloat");
-    println!("cargo:rustc-link-lib=static=gecodedriver");
-    println!("cargo:rustc-link-lib=static=gecodeminimodel");
-    
     println!("cargo:rustc-link-lib=static=backend");
+  
+    // needed for gecode
+    // order matters for g++!
+    println!("cargo:rustc-link-lib=static=gecodeminimodel");
+    println!("cargo:rustc-link-lib=static=gecodedriver");
+    println!("cargo:rustc-link-lib=static=gecodefloat");
+    println!("cargo:rustc-link-lib=static=gecodeset");
+    println!("cargo:rustc-link-lib=static=gecodeint");
+    println!("cargo:rustc-link-lib=static=gecodesearch");
+    println!("cargo:rustc-link-lib=static=gecodekernel");
+    println!("cargo:rustc-link-lib=static=gecodesupport");
     
     tauri_build::build()
 }
